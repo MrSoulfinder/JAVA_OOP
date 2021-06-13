a71af026c555d89ebe0400b490d6743519a3c108
MrSoulfinder <57533125+MrSoulfinder@users.noreply.github.com>
2021-06-13 17:15:03 +0300

    Add files via upload

---
+++ Collections/src/collections/Collections.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();  
+        ArrayList <String> list2 = new ArrayList <String>();
+        ArrayList <String> list3 = new ArrayList <>();
+        
+        list3.add("hello");
+        list3.add("world!");
+        System.out.print(list3);
+    }
+    
+}
